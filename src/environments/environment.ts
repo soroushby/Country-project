bebc477f7a6eb8601578fce572e0e8e48a4ccb2a
Soroushby <sorosh.bayanati@yahoo.com>
2022-11-06 06:43:42 -0700

    added firebase to project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCen-ObmiAa4dzeWLnbF68vK5En8Nif0IQ',
+    authDomain: 'countries-2255a.firebaseapp.com',
+    projectId: 'countries-2255a',
+    storageBucket: 'countries-2255a.appspot.com',
+    messagingSenderId: '464462637703',
+    appId: '1:464462637703:web:b8c6ce18906e09782ba622',
+    measurementId: 'G-BLYM3RF0G9',
+  },
 };
 
 /*
